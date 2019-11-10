7584455fad8cc328b00f8751b5dac59b4cb1e21c
Lee-Zong-Yang <100073484@students.swinburne.edu.my>
2019-11-10 14:20:27 +0800

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyGame;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        DiningTable _diningTable = new DiningTable();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _diningTable.SetX(0);
+            Assert.AreEqual(_diningTable.DiningTableSprite.X, 0);
+        }
+    }
+}
