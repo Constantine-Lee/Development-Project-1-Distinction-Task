b4ed3d315f696024f06ca9a1795da66dc4787ea0
B-A-G-S <100071239@students.swinburne.edu.my>
2019-11-16 21:33:00 +0800

    Unit testing

---
+++ UnitTestProject1/BS_Test1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyGame;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BS_Test1
+    {
+        BS_SideBar _sideBar = new BS_SideBar();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(_sideBar.FullLifeCheck(), true);
+        }
+    }
+}
