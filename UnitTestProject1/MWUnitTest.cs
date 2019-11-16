2800e08e977fa31ab49d0372652389d3bc9d8175
MichaelWong97 <michaelwonggengwu@hotmail.com>
2019-11-16 22:33:56 +0800

    Add MWDiningTableSetX test

--- UnitTestProject1/MWUnitTest.cs
+++ UnitTestProject1/MWUnitTest.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyGame;
 
 namespace UnitTestProject1
 {
     [TestClass]
-    public class MWTestButton
+    public class MWUnitTest
     {
+        MWDiningTable _diningtable = new MWDiningTable();
+
         [TestMethod]
         public void TestMethod1()
         {
+            _diningtable.SetX(0);
+            Assert.AreEqual(_diningtable.DiningTableSprite,10);
         }
     }
 }
