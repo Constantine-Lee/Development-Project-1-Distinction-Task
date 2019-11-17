18324971440f00ef0ea461482694fd4aa9577eab
MichaelWong97 <michaelwonggengwu@hotmail.com>
2019-11-17 19:26:29 +0800

    Fix GameMode error

--- MW/Menu/GameMode.cs
+++ MW/Menu/GameMode.cs
@@ -6,9 +6,6 @@ namespace MyGame
 	public class GameMode : View
 	{
 		private MWButton _classicButton;
-		private MWButton _survivalButton;
-		private MWButton _casualButton;
-		private MWButton _gotoMenuButton;
 
 		private Sprite _gameMode;
 		private string _backgroundImage;
@@ -26,30 +23,47 @@ namespace MyGame
 			_classicButton.SetHeight (49);
 			_classicButton.SetText (" Classic ", 30);
 
-			_survivalButton = new MWButton ("grey_button06.png");
-			_survivalButton.SetWidth (191);
-			_survivalButton.SetHeight (49);
-			_survivalButton.SetText (" Survival ", 30);
-
-			_casualButton = new MWButton ("grey_button06.png");
-			_casualButton.SetWidth (191);
-			_casualButton.SetHeight (49);
-			_casualButton.SetText (" Casual ", 30);
-
-			_gotoMenuButton = new MWButton ("grey_button06.png");
-			_gotoMenuButton.SetWidth (191);
-			_gotoMenuButton.SetHeight (49);
-			_gotoMenuButton.SetText (" Back to Menu ", 25);
 		}
 
 		public override void Draw ()
 		{
 			SwinGame.DrawSprite (_gameMode);
 			_classicButton.Draw2 ();
-			_survivalButton.Draw2 ();
-			_casualButton.Draw2 ();
-			_gotoMenuButton.Draw ();
 		}
-	}
+
+        public override void Update()
+        {
+        }
+
+        public override void ProcessEvent()
+        {
+            if (SwinGame.MouseClicked(MouseButton.LeftButton))
+            {
+                if (_classicButton.IsAt(SwinGame.MousePosition()))
+                {
+                    _viewManager.View = _viewManager.MWNewClassicGame();
+                }
+            }
+            if (SwinGame.KeyTyped(KeyCode.vk_F12))
+            {
+                SwinGame.ToggleFullScreen();
+            }
+        }
+
+
+        //x = 0
+        public void SetX(int x)
+        {
+            _classicButton.SetX(x + 90);
+            _classicButton.SetTextPositionX(x + 128);
+        }
+
+        //y = 0
+        public void SetY(int y)
+        {
+            _classicButton.SetY(y + 100);
+            _classicButton.SetTextPositionY(y + 108);
+        }
+    }
 }
 
