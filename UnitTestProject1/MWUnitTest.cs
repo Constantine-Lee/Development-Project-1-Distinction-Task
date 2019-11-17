5612f4db311519abf7e04ef65b5038223e6984e9
MichaelWong97 <michaelwonggengwu@hotmail.com>
2019-11-18 00:31:19 +0800

    Adding UnitTest for GameMode.cs

--- UnitTestProject1/MWUnitTest.cs
+++ UnitTestProject1/MWUnitTest.cs
@@ -32,5 +32,16 @@ namespace UnitTestProject1
             Assert.AreEqual(_holdingfoodframe.HoldingFood.X, 384);
             Assert.AreEqual(_holdingfoodframe.HoldingFood.Y, 178);
         }
+
+        [TestMethod]
+        public void TestGameModeSetX()
+        {
+            ViewManager _viewManager = new ViewManager();
+            GameMode _gameMode = new GameMode(_viewManager);
+            MWButton _button = new MWButton("grey_button06.png");
+
+            _gameMode.SetX(0);
+            Assert.AreEqual(_gameMode.ClassicButton.TextPosX, 128);
+        }
     }
 }
