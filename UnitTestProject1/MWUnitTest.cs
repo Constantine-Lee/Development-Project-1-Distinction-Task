1e1db103f47ff879341aa7ab0f90b6131e85660c
Lee-Zong-Yang <100073484@students.swinburne.edu.my>
2019-11-19 02:15:33 +0800

    Add back ZYUnitTest, Remove all test in MWUnitTest

--- UnitTestProject1/MWUnitTest.cs
+++ UnitTestProject1/MWUnitTest.cs
@@ -7,37 +7,6 @@ namespace UnitTestProject1
     [TestClass]
     public class MWUnitTest
     {
-        MWDiningTable _diningtable = new MWDiningTable();
-        MWWoodenTable _woodentable = new MWWoodenTable();
 
-        [TestMethod]
-        public void TestDiningTableSetX()
-        {
-            _diningtable.SetX(0);
-            Assert.AreEqual(_diningtable.DiningTableSprite.X, 0);
-        }
-
-        [TestMethod]
-        public void TestWoodenTableSetX()
-        {
-            _woodentable.SetX(0);
-            Assert.AreEqual(_woodentable.DiningTableSprite.X, 0);
-        }
-
-        [TestMethod]
-        public void TestSideBarUpdateHoldingFoodFrame()
-        {
-            MWHoldingFoodFrame _holdingfoodframe = new MWHoldingFoodFrame();
-            Assert.AreEqual(_holdingfoodframe.HoldingFood.X, 384);
-            Assert.AreEqual(_holdingfoodframe.HoldingFood.Y, 178);
-        }
-
-        [TestMethod]
-        public void TestSideBarUpdateScore()
-        {
-            MWSideBarUpdate __sidebarUpdate = new MWSideBarUpdate();
-            __sidebarUpdate.AddScore();
-            Assert.AreEqual(100, MWSideBarUpdate.Score);
-        }
     }
 }
