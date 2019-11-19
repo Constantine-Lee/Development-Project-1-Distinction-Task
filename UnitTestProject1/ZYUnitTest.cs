00da485e85d57e52c3ce790d254386f182fa0d3b
Lee-Zong-Yang <100073484@students.swinburne.edu.my>
2019-11-19 19:54:56 +0800

    Revert "remove all test in ZYUnitTest"
    This reverts commit f01064a4ce42ba1ad5718661d2a5875286fc749f.

--- UnitTestProject1/ZYUnitTest.cs
+++ UnitTestProject1/ZYUnitTest.cs
@@ -7,6 +7,34 @@ namespace UnitTestProject1
     [TestClass]
     public class ZYUnitTest
     {
+        ZYSampleFood zYSampleFood = new ZYSampleFood("samplefood.png");
+        [TestMethod]
+        public void TestSampleFood()
+        {
+            Assert.AreEqual("samplefood.png", zYSampleFood.Image);
+            Assert.AreNotEqual("false", zYSampleFood.Image);
+        }
 
+        [TestMethod]
+        public void TestGameLife()
+        {
+            ZYGameLife zYGameLife = new ZYGameLife();
+            Assert.AreEqual(100, zYGameLife.Width);
+        }
+
+        [TestMethod]
+        public void TestPauseScreen()
+        {
+            Boolean result;
+            try
+            {
+                PauseScreen pauseInterface = new PauseScreen();
+                result = true;
+            }catch(Exception e)
+            {
+                result = false;
+            }
+            Assert.AreEqual(true, result);
+        }
     }
 }
