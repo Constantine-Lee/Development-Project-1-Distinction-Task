fe016c1219ecca987e976cf63a170866418f347e
Lee-Zong-Yang <100073484@students.swinburne.edu.my>
2019-11-19 19:59:39 +0800

    check pipeline working or not

--- UnitTestProject1/ZYUnitTest.cs
+++ UnitTestProject1/ZYUnitTest.cs
@@ -11,7 +11,7 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestSampleFood()
         {
-            Assert.AreEqual("samplefood.png", zYSampleFood.Image);
+            Assert.AreEqual("false", zYSampleFood.Image);
             Assert.AreNotEqual("false", zYSampleFood.Image);
         }
 
