4359aa4fdfe075fdc247540b8ee6db4deb11c21f
MichaelWong97 <michaelwonggengwu@hotmail.com>
2019-11-29 01:09:52 +0800

    Fix build error again for test

--- MW/Game/MWmarioGame.cs
+++ MW/Game/MWmarioGame.cs
@@ -11,10 +11,11 @@ namespace MyGame
         private MWSideBarMarioUpdate _mariosideBar;
         private MWBottomBar _mariobtmBar;
         private MWButton _mariogiveUpButton;
-
+        */
         public MWmarioGame(ViewManager viewManager)
             : base(viewManager)
         {
+            /*
             _mariosideBar = new MWSideBarMarioUpdate(_viewManager);
             _marioservingArea = new MWMarioServingArea();
             _mariobtmBar = new MWBottomBar();
@@ -32,8 +33,10 @@ namespace MyGame
             _mariobtmBar.RegisterStove(_marioservingArea.Stoves);
             _marioservingArea.Player.RegisterHoldingFrame(_mariosideBar.HoldingFoodFrame);
             //
+            */
         }
 
+        /*
         public override void Draw()
         {
             _marioservingArea.Draw();
@@ -85,6 +88,6 @@ namespace MyGame
             _mariogiveUpButton.SetY(y + 295);
             _mariogiveUpButton.SetTextPositionY(y + 310);
         }
-*/
+        */
     }
 }
