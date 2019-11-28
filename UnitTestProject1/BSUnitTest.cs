07e7c653034eeedfa8f6e3ce23b613dc6cc64cb7
B-A-G-S <100071239@students.swinburne.edu.my>
2019-11-28 20:24:52 +0800

    Checking For Sound effects files (TDD)

--- UnitTestProject1/BSUnitTest.cs
+++ UnitTestProject1/BSUnitTest.cs
@@ -8,17 +8,41 @@ namespace UnitTestProject1
     [TestClass]
     public class BS_Test1
     {
-       
+
         [TestMethod]
         public void WalkingSoundFilePresenceTest()
         {
-           bool check = File.Exists("Resources/sounds/walking.ogg");
+            bool check = File.Exists("Resources/sounds/walking.ogg");
             Assert.IsTrue(check);
         }
 
         [TestMethod]
         public void LowHPSoundFilePresenceTest() {
             bool check = File.Exists("Resources/sounds/low_hp.wav");
+            Assert.IsTrue(check);
+        }
+
+        [TestMethod]
+        public void SellItemSoundEffectTest() {
+            bool check = File.Exists("Resources/sounds/sell_buy_item.wav");
+            Assert.IsTrue(check);
+        }
+
+        [TestMethod]
+        public void CandyPickUpSoundEffectTest() {
+            bool check = File.Exists("Resources/sounds/ring_inventory.wav");
+            Assert.IsTrue(check);
+        }
+
+        [TestMethod]
+        public void candyTrashTest() {
+            bool check = File.Exists("Resources/sounds/cloth-inventory.wav");
+            Assert.IsTrue(check);
+        }
+
+        [TestMethod]
+        public void gainPackTest() {
+            bool check = File.Exists("Resources/images/GainPack.png");
             Assert.IsTrue(check);
         }
     }
