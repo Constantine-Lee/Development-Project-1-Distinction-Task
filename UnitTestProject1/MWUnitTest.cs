c5b95c8da5ffe56470e928e3bb38431be5b1a0de
MichaelWong97 <michaelwonggengwu@hotmail.com>
2019-11-30 00:54:05 +0800

    TDD test on MarioDiningTable

--- UnitTestProject1/MWUnitTest.cs
+++ UnitTestProject1/MWUnitTest.cs
@@ -73,5 +73,15 @@ namespace UnitTestProject1
             bool check1 = File.Exists("MW/Game/MWmarioGame.cs");
             Assert.IsTrue(check1);
         }
+
+        [TestMethod]
+        public void TestMarioDiningTable()
+        {
+            MWMarioDiningTable mWMarioDiningTable = new MWMarioDiningTable();
+            MWMarioWoodenTable mWMarioWoodenTable = new MWMarioWoodenTable();
+            
+            Assert.IsNotNull(mWMarioDiningTable);
+            Assert.IsNotNull(mWMarioWoodenTable);
+        }
     }
 }
